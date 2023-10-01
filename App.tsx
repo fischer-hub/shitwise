d717d7e848abf7187518eae76f82bb17669c0232
David Fischer <davidfischer99@gmx.de>
2023-10-01 09:51:18 +0200

    some old commmit

--- App.tsx
+++ App.tsx
@@ -1,35 +1,36 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Image } from 'react-native'
 import NextShitterComponent from './src/components/NextShitterComponent'
 import ShitListComponent from './src/components/ShitListComponent'
 import { compareAsc } from 'date-fns'
+import AddShitBtnComponent from './src/components/AddShitBtnComponent'
+import StatusBarComponent from './src/components/StatusBarComponent'
+import 'react-native-url-polyfill/auto'
+import { shitwiseDB } from './src/components/supabaseConfig'
 
 
 type shitListEntry = {
-  date: Date;
-  title: string;
+  dateBegin: Date | undefined;
+  dateEnd: Date | undefined;
+  name: string;
 };
 
 const compShitListEntries = ( a: shitListEntry, b: shitListEntry ) => {
-  return compareAsc(a.date, b.date);
+  a.dateBegin ??= new Date()
+  b.dateBegin ??= new Date()
+  return compareAsc(a.dateBegin, b.dateBegin);
 };
+
+
+export const channel = shitwiseDB.channel('schema-db-changes').on('postgres_changes', { event: 'INSERT', schema: 'public'}, (payload) => 
+  console.log(payload)
+  ).subscribe()
 
 const DATA = [
   {
-    date: new Date(1995, 6, 2),
-    title: 'Yoyo',
-  },
-  {
-    date: new Date(2023, 7, 2),
-    title: 'Kerst',
-  },
-  {
-    date: new Date(2024, 8, 2),
-    title: 'Lulu',
-  },
-  {
-    date: new Date(2012, 9, 9),
-    title: 'Lali',
+    dateBegin: new Date(1995, 6, 2),
+    dateEnd: new Date(1995, 6, 5),
+    name: 'Yoyi',
   },
 ];
 
@@ -37,14 +38,15 @@ const App = () => {
 
   return(
     <View style={styles.container}>
+      <StatusBarComponent/>
       <View style={styles.nextShitterDisplay}>
         <NextShitterComponent shitList = {DATA.sort(compShitListEntries)}/>
       </View>
       <View style={styles.shitListDisplay}>
         <ShitListComponent shitList = {DATA.sort(compShitListEntries)}/>
       </View>
+      <AddShitBtnComponent/>
     </View>
-
   )
 }
 
@@ -53,10 +55,10 @@ const styles = StyleSheet.create({
     flex: 1
   },
   nextShitterDisplay: {
-    flex: 1
+    flex: 35
   },
   shitListDisplay: {
-    flex: 2
+    flex: 65
   }
 })
 export default App
